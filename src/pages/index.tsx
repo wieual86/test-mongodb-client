c124945ec55ef5098f9320bd9aed031cb76b1fbd
Eugene Allen <narrativeengineers@gmail.com>
2023-12-07 09:41:13 -0500

    Latest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,1 @@
-export { default } from "sitePage/components";
+export { default } from "user/components";
--- src/user/components/index.ts
+++ src/user/components/index.ts
@@ -1,3 +1,2 @@
 export { default } from "./UserPage";
-export { default as UserHeader } from "./UserHeader";
 export { default as LoginRedirect } from "./LoginRedirect";
